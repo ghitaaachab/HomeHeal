ed8b49a08ff9e8dcf478c7b01a268b090f2420e9
Ghita Aachab <ghita.aachab@gmail.com>
2024-01-23 12:18:46 +0100

    configured devise with the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # before_action :configure_permitted_parameters, :set_no_navbar, if: :devise_controller?
+  # # before_action :set_time_zone, if: :user_signed_in?
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  # end
+  before_action :authenticate_user!
+
 end
