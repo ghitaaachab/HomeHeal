5d20f7b51d9a7b9110ff6f20904e14e3fb3dfec2
Amine Bagui <med.bagui.1@gmail.com>
2024-01-26 14:26:41 +0100

    flatpickr configuration & issues with js & stimilus fixed with the help of TAs

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"
 
+// Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
     flatpickr(this.element);
