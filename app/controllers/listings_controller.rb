a2908c0963d9bbc11582022a408d0554001e523e
Amine Bagui <med.bagui.1@gmail.com>
2024-01-26 14:54:29 +0100

    date is now picked up from flatpickr

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -14,17 +14,17 @@ class ListingsController < ApplicationController
   end
 
   def book_appointment
-  client = Client.find_by(user_id: current_user.id)
-  hcp = Hcp.find(params[:id])
+    client = Client.find_by(user_id: current_user.id)
+    hcp = Hcp.find(params[:id])
 
-  appointment = Appointment.new(client_id: client.id, hcp_id: hcp.id, date: Date.today, status: "Pending")
+    appointment = Appointment.new(client_id: client.id, hcp_id: hcp.id, date: params[:appointment][:date], status: "Pending")
 
-  if appointment.save
-    redirect_to appointments_path
-  else
-    flash[:alert] = "Failed to book the appointment."
+    if appointment.save
+      redirect_to appointments_path
+    else
+      flash[:alert] = "Failed to book the appointment."
+    end
   end
-end
 
   private
 
